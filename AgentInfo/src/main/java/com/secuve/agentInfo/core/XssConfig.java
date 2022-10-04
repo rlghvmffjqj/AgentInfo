ae1c8b2232d0b4f8d6e54a2b8e585b73290ecb01
rlghvmffjqj <rlghvmffjqj@naver.com>
2022-10-04 17:46:43 +0900

    2022-10-04 XSS 공격 예방 범위 수정

--- AgentInfo/src/main/java/com/secuve/agentInfo/core/XssConfig.java
+++ AgentInfo/src/main/java/com/secuve/agentInfo/core/XssConfig.java
@@ -1,11 +1,20 @@
 package com.secuve.agentInfo.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 
+/**
+ * XSS 공격 예방 ISSUE 패이지의 경우 이미지를 저장하기 때문에 제외
+ * @author JiEx
+ *
+ */
 @Configuration
 public class XssConfig implements WebMvcConfigurer {
 	@Bean
@@ -13,7 +22,19 @@ public class XssConfig implements WebMvcConfigurer {
 		FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
 		filterRegistration.setFilter(new XssEscapeServletFilter());
 		filterRegistration.setOrder(1);
-		filterRegistration.addUrlPatterns("/*");
+		//filterRegistration.addUrlPatterns("/*");
+		List<String> urls = new ArrayList<>();
+		urls.add("/packages/*");
+		urls.add("/license/*");
+		urls.add("/serverList/*");
+		urls.add("/generalPackage/*");
+		urls.add("/customPackage/*");
+		urls.add("/employee/*");
+		urls.add("/category/*");
+		urls.add("/requests/*");
+		urls.add("/requestsWrite/*");
+		filterRegistration.setUrlPatterns(urls);
 		return filterRegistration;
 	}
+	
 }
