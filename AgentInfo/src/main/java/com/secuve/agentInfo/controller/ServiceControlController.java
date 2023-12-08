2cab0499d83e2df507662e250299933204de1a7d
kiho-1 <kiho-1@kiho-1>
2023-12-08 18:18:25 +0900

    2023-12-08 tomcat 로그

--- AgentInfo/src/main/java/com/secuve/agentInfo/controller/ServiceControlController.java
+++ AgentInfo/src/main/java/com/secuve/agentInfo/controller/ServiceControlController.java
@@ -108,61 +108,6 @@ public class ServiceControlController {
 	@ResponseBody
 	@PostMapping(value = "/serviceControl/update")
 	public String ServiceControlUpdate(ServiceControl serviceControl) {
-		StringBuffer result = new StringBuffer(); // 성공 스트링 버퍼
-		// 로그 파일 경로
-        String filePath = "D:/catalina.out";
-        // 특정 날짜 이후의 로그를 필터링하려는 날짜
-        String targetDateString = "2023-12-07 16:33:58";
-
-        try {
-            // SimpleDateFormat을 사용하여 문자열을 Date 객체로 변환
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss.SSS");
-
-            // 정규 표현식 패턴 생성
-            Pattern datePattern = Pattern.compile("\\d{2}-[a-zA-Z]{3}-\\d{4} \\d{2}:\\d{2}:\\d{2}\\.\\d{3}");
-
-            Date targetDate = dateFormat.parse(targetDateString);
-
-            // 로그 파일을 읽기 위해 Scanner 사용
-            Scanner scanner = new Scanner(new File(filePath));
-
-            // 특정 날짜 이후의 로그를 출력하는 플래그
-            boolean printLogs = false;
-
-            // 각 라인을 읽어와서 처리
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-
-                // 정규 표현식으로 날짜를 찾기
-                Matcher matcher = datePattern.matcher(line);
-                if (matcher.find()) {
-                    String logDateString = matcher.group();
-                    try {
-                        // 날짜를 Date 객체로 변환하여 특정 날짜와 비교
-                        Date logDate = dateFormat.parse(logDateString);
-                        if (logDate.equals(targetDate) || logDate.after(targetDate)) {
-                            printLogs = true;
-                        }
-                    } catch (ParseException e) {
-                        // 날짜를 파싱하는 중에 오류가 발생하면 무시하고 계속 진행
-                    }
-                }
-
-                // 특정 날짜 이후의 로그를 출력
-                if (printLogs) {
-                    System.out.println(line);
-                }
-            }
-
-            // Scanner 닫기
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.err.println("로그 파일을 찾을 수 없습니다: " + filePath);
-        } catch (ParseException e) {
-            System.err.println("날짜를 처리하는 중에 오류가 발생했습니다: " + e.getMessage());
-        }
-
-		return result.toString();
-		//return serviceControlService.setServiceControlUpdate(serviceControl);
+		return serviceControlService.setServiceControlUpdate(serviceControl);
 	}
 }
